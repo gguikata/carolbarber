6a90b9a920007c431ab7f0ac8fc9387cb1abf7bf
Guilherme <guikatayama.gt@gmail.com>
2024-03-09 20:33:26 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'carolbarber';
+  title = 'barbeira-carol';
 }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class MainComponent {
+
+}
